6d68b60bca4c8f10ebfe981c6243ddbe3fc33975
MooiBlack <mooimosho@hotmail.com>
2020-10-13 22:41:53 -0500

    Se añadieron los archivos java, ya muestra menú y repite, tambien guarda y rota proceso en la cola

---
+++ ColaPrioridad.java
@@ -0,0 +1,119 @@
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.Random;
+
+/**
+ *Esta es la primera ṕráctica de sistemas operativos
+ *@author	Mariana Martínez Soto.
+ *@version	0.1
+ *@since  	1.0
+*/
+/*Puede ser cola de prioridad, dado que depende el número  id que tenga*
+*/
+public class ColaPrioridad{
+	public static final String ANSI_GREEN = "\u001B[32m"; /*Colores*/
+	public static final String ANSI_RESET = "\u001B[0m";  /*Colores */ 
+	public static final String ANSI_BLUE = "\u001B[34m";  /*Colores */
+	public static final String ANSI_RED = "\u001B[31m";  /*Colores */
+
+	int cont_memoria; /* Contador de memoria ó memoria ocupada*/
+	int cant_memoria; /* Cantidad de memoria*/
+	int cant_mem_temp;
+	int cant_instr_proc; /*	Cantidad de instrucciones*/
+
+
+	public void Imprimir(){
+		System.out.println(cant_memoria);
+	}
+	public ColaPrioridad(int mem){
+		this.cant_memoria=cant_memoria;
+	}
+	public int MemAleatoria(){
+		int mem_aleatoria;
+		Random r = new Random();
+		mem_aleatoria = r.nextInt(7-4+1)+4;
+		return mem_aleatoria;
+	}
+	public int InstrAleatoria(){
+		int inst_aleatoria;
+		Random r = new Random();
+		cant_instr_proc = r.nextInt(30-10+1)+10;
+		return cant_instr_proc;
+	}
+
+	/*---------------------------------------------------------*/
+	public static void main(String args[]){
+
+	PriorityQueue<proceso> cola = new PriorityQueue<>();
+	ColaPrioridad memoria = new ColaPrioridad(2048);
+	
+		int option;
+		String nombre_proc;
+		int mem_temp;
+		Scanner leer = new Scanner(System.in);
+
+
+
+		do{
+		System.out.println('\n');
+		System.out.println("1. Crecar proceso");
+		System.out.println("2. Ver estado actual");
+		System.out.println("3.Imprimir cola de proceso");
+		System.out.println("4. Ejecutar proceso actual");
+		System.out.println("5. Ver proceso actual");
+		System.out.println("6. Pasar al proceso siguiente");
+		System.out.println("7. Matar el proceso actual");
+		System.out.println("8. Matar todos los procesos y terminar");
+		
+		option=leer.nextInt();	//Entrada de opción
+
+		switch(option) {
+			case 1: System.out.println(ANSI_BLUE+"Ingresa nombre de proceso"+ANSI_RESET);
+					Scanner nombre = new Scanner(System.in);
+					nombre_proc=nombre.nextLine();
+					memoria.cant_mem_temp=(int) Math.pow(2, memoria.MemAleatoria());
+					if (memoria.cant_mem_temp+memoria.cont_memoria > 2048){
+						System.out.println(ANSI_RED+"La memoria está llena"+ANSI_RESET);
+					}
+					cola.add(new proceso(nombre_proc,1,memoria.InstrAleatoria(),memoria.cant_mem_temp));
+					memoria.cont_memoria=memoria.cont_memoria+memoria.cant_mem_temp;
+					break;
+
+			case 2: System.out.println(ANSI_GREEN+"Procesos listos: "+cola.size()+ANSI_RESET);
+					break;
+
+			case 3: 
+					break;
+
+			case 4:	proceso proceso_temporal = cola.peek();
+					proceso_temporal.instr=proceso_temporal.instr-5;
+					cola.add(cola.poll());
+					System.out.println(ANSI_GREEN+"Procesos listos: "+cola.size()+ANSI_RESET);
+					System.out.println(ANSI_GREEN+"instrucciones: "+proceso_temporal.instr+ANSI_RESET);
+					break;
+
+			case 5: System.out.println(ANSI_GREEN+"Proceso actual: "+'\n'+"Nombre: "+(cola.peek()).nombre+'\n'+"ID: "+(cola.peek()).id+'\n'+"N° instrucciones: "+(cola.peek()).instr+'\n'+"Espacio memoria: "+(cola.peek()).espacio_mem+ANSI_RESET);
+					break;
+
+			case 6:
+					break;
+
+			case 7:
+					break;
+
+			case 8: 
+					break;
+
+			default: System.out.println(ANSI_RED+"No se permite opción"+ANSI_RESET);
+		}
+
+		}while(option != 8);
+
+		if (cola.peek()!=null){
+			System.out.println(ANSI_BLUE+"Hay por lo menos un proceso en espera"+ANSI_RESET);
+		}
+	}
+}
---
+++ proceso.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+import java.util.PriorityQueue;
+
+class proceso implements Comparable<proceso>{
+	String nombre;
+	int id;
+	int instr;
+	int espacio_mem;
+
+	public proceso(String nombre, int id, int instr, int espacio_mem){
+		this.nombre=nombre;
+		this.id=id;
+		this.instr=instr;
+		this.espacio_mem=espacio_mem;
+	}
+
+	public void setName(String nombre){
+		this.nombre=nombre;
+	}
+
+	public String getName(){
+		return nombre;
+	}
+
+	public int get_id(){
+		return id;
+	}
+	public int instr(){
+		return instr;
+	}
+	public int espacio_mem(){
+		return espacio_mem;
+	}
+
+	public void excute_proc(){
+		this.instr=this.instr-5;
+	}
+
+	public int compareTo(proceso process){
+		if(this.get_id() > process.get_id()){
+			return 1;
+		}
+		return 0;
+	}
+
+
+
+}
